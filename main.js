a2b295e6e5f14dc5a4bf731449355bd102b0d5b2
wmackin <wmackin@live.com>
2022-12-31 16:47:22 -0500

    Added search by IMDB ID

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 document.getElementById('searchMovie').addEventListener('click', async e => {
-
     const options = {
         method: 'GET',
         headers: {
@@ -22,7 +21,33 @@ document.getElementById('searchMovie').addEventListener('click', async e => {
             }
         }
         else {
-            content = 'No results.'
+            content = '<p>No results.</p>'
+        }
+        document.getElementById('searchOutput').innerHTML = content;
+    }
+});
+
+document.getElementById('searchID').addEventListener('click', async e => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c6609e233bmsh4537c819c3a8d4cp198905jsn3792519320e0',
+            'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+        }
+    };
+    const movieID = document.getElementById('idSearch').value;
+    const response = await fetch(`https://movie-database-alternative.p.rapidapi.com/?i=${movieID}`, options);
+    let content = '';
+    if (response.ok) {
+        const data = await response.json();
+        if (data["Response"] === "True") {
+            const img = data["Poster"];
+            const name = data["Title"];
+            const year = data["Year"];
+            content += `<img src=${img}><h1>${name} (${year})</h1>`;
+        }
+        else {
+            content = '<p>No results.</p>'
         }
         document.getElementById('searchOutput').innerHTML = content;
     }
