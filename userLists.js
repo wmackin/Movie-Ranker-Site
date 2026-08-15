904d1d0fbe227915cf1e8d043a8f2fa2bfab0878
wmackin <wmackin@live.com>
2026-08-14 20:38:31 -0400

    Minor fixes.

--- userLists.js
+++ userLists.js
@@ -36,174 +36,171 @@ document.getElementById('createList').addEventListener('click', async () => {
     }
 });
 
-document.getElementById('deleteList').addEventListener('click', async () => {
-    const listName = document.getElementById('userLists').value;
-    if (window.confirm("Delete " + listName)) {
-        await fetch('/deleteList', {
+document.getElementById('enterRanking').addEventListener('click', async () => {
+    const mode = document.getElementById('rankOptions').value;
+    if (mode == 'View as Grid') {
+        const listName = document.getElementById('userLists').value;
+        const tagsFiltered = document.getElementById('tagFilter').value;
+        const tagsList = tagsFiltered.split(',')
+        console.log(tagsList)
+        const response = await fetch('/getList', {
             method: "POST",
             redirect: 'follow',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ listName: listName }),
-        });
-        location.reload();
-    }
-});
-
-document.getElementById('resetList').addEventListener('click', async () => {
-    const listName = document.getElementById('userLists').value;
-    if (window.confirm("Reset " + listName)) {
-        await fetch('/resetList', {
-            method: "POST",
-            redirect: 'follow',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ listName: listName }),
-        });
-    }
-});
-
-document.getElementById('renameList').addEventListener('click', async () => {
-    const listName = document.getElementById('userLists').value;
-    const newListName = window.prompt("Enter new list name");
-    if (newListName !== null && newListName.length > 0) {
-        const response = await fetch('/renameList', {
-            method: "POST",
-            redirect: 'follow',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ oldListName: listName, newListName: newListName }),
         });
         if (response.ok) {
-            const results = await response.json();
-            if (results['success']) {
-                location.reload();
+            const list = await response.json();
+            const viewingDiv = document.getElementById('viewingDiv');
+            viewingDiv.classList.add("grid-container");
+            while (viewingDiv.firstChild) {
+                viewingDiv.removeChild(viewingDiv.firstChild);
             }
-            else {
-                document.getElementById('confirmationMessage').innerHTML = results['msg'];
-            }
+            list.forEach(m => {
+                const itemTags = m['tags'].split(',')
+                if (tagsFiltered.length === 0 || tagsList.every(t => itemTags.includes(t))) {
+                    const imgNode = document.createElement('img');
+                    imgNode.src = m['poster'];
+                    imgNode.classList.add('image-display')
+                    const newDiv = document.createElement('div');
+                    newDiv.classList.add('grid-item');
+                    newDiv.appendChild(imgNode);
+                    viewingDiv.appendChild(newDiv);
+                }
+            })
         }
     }
-});
-
-document.getElementById('viewGrid').addEventListener('click', async () => {
-    const listName = document.getElementById('userLists').value;
-    const tagsFiltered = document.getElementById('tagFilter').value;
-    const tagsList = tagsFiltered.split(',')
-    console.log(tagsList)
-    const response = await fetch('/getList', {
-        method: "POST",
-        redirect: 'follow',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ listName: listName }),
-    });
-    if (response.ok) {
-        const list = await response.json();
-        const viewingDiv = document.getElementById('viewingDiv');
-        viewingDiv.classList.add("grid-container");
-        while (viewingDiv.firstChild) {
-            viewingDiv.removeChild(viewingDiv.firstChild);
-        }
-        list.forEach(m => {
-            const itemTags = m['tags'].split(',')
-            if (tagsFiltered.length === 0 || tagsList.every(t => itemTags.includes(t))) {
-                const imgNode = document.createElement('img');
-                imgNode.src = m['poster'];
-                imgNode.classList.add('image-display')
-                const newDiv = document.createElement('div');
-                newDiv.classList.add('grid-item');
-                newDiv.appendChild(imgNode);
-                viewingDiv.appendChild(newDiv);
+    else if (mode == 'View as List') {
+        const listName = document.getElementById('userLists').value;
+        const tagsFiltered = document.getElementById('tagFilter').value;
+        const tagsList = tagsFiltered.split(',')
+        const response = await fetch('/getList', {
+            method: "POST",
+            redirect: 'follow',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ listName: listName }),
+        });
+        if (response.ok) {
+            const list = await response.json();
+            const viewingDiv = document.getElementById('viewingDiv');
+            viewingDiv.classList.remove("grid-container");
+            while (viewingDiv.firstChild) {
+                viewingDiv.removeChild(viewingDiv.firstChild);
             }
-        })
-    }
-});
-
-document.getElementById('viewList').addEventListener('click', async () => {
-    const listName = document.getElementById('userLists').value;
-    const tagsFiltered = document.getElementById('tagFilter').value;
-    const tagsList = tagsFiltered.split(',')
-    const response = await fetch('/getList', {
-        method: "POST",
-        redirect: 'follow',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ listName: listName }),
-    });
-    if (response.ok) {
-        const list = await response.json();
-        const viewingDiv = document.getElementById('viewingDiv');
-        viewingDiv.classList.remove("grid-container");
-        while (viewingDiv.firstChild) {
-            viewingDiv.removeChild(viewingDiv.firstChild);
-        }
-        for (let i = 0; i < list.length; i++) {
-            const itemTags = list[i]['tags'].split(',')
-            if (tagsList.length === 0 || tagsList.every(t => itemTags.includes(t))) {
-                const textNode = document.createElement('h3');
-                textNode.innerHTML = (i + 1).toString() + '. ' + list[i]['title']
-                const newDiv = document.createElement('div');
-                newDiv.appendChild(textNode);
-                viewingDiv.appendChild(newDiv);
+            for (let i = 0; i < list.length; i++) {
+                const itemTags = list[i]['tags'].split(',')
+                if (tagsFiltered.length === 0 || tagsList.every(t => itemTags.includes(t))) {
+                    const textNode = document.createElement('h3');
+                    textNode.innerHTML = (i + 1).toString() + '. ' + list[i]['title']
+                    const newDiv = document.createElement('div');
+                    newDiv.appendChild(textNode);
+                    viewingDiv.appendChild(newDiv);
+                }
             }
         }
     }
-});
-
-document.getElementById('rankButton').addEventListener('click', async () => {
-    const listName = document.getElementById('userLists').value;
-    const response = await fetch('/rankList', {
-        method: "POST",
-        redirect: 'follow',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ listName: listName }),
-    });
-    if (response.ok) {
-        if (response.redirected) {
-            window.location.assign(response.url);
+    else if (mode == "Rank") {
+        const listName = document.getElementById('userLists').value;
+        const response = await fetch('/rankList', {
+            method: "POST",
+            redirect: 'follow',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ listName: listName }),
+        });
+        if (response.ok) {
+            if (response.redirected) {
+                window.location.assign(response.url);
+            }
         }
     }
-});
-
-document.getElementById('rankTopButton').addEventListener('click', async () => {
-    const listName = document.getElementById('userLists').value;
-    const response = await fetch('/rankListTop', {
-        method: "POST",
-        redirect: 'follow',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ listName: listName }),
-    });
-    if (response.ok) {
-        if (response.redirected) {
-            window.location.assign(response.url);
+    else if (mode == "Rank Top Movies") {
+        const listName = document.getElementById('userLists').value;
+        const response = await fetch('/rankListTop', {
+            method: "POST",
+            redirect: 'follow',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ listName: listName }),
+        });
+        if (response.ok) {
+            if (response.redirected) {
+                window.location.assign(response.url);
+            }
         }
     }
-});
-
-document.getElementById('rankTopishButton').addEventListener('click', async () => {
-    const autorankScore = window.parseInt(window.prompt('Select a score for autoranking score threshold'));
-    const listName = document.getElementById('userLists').value;
-    const response = await fetch('/rankListTopish', {
-        method: "POST",
-        redirect: 'follow',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ listName: listName, autorankThreshold: autorankScore }),
-    });
-    if (response.ok) {
-        if (response.redirected) {
-            window.location.assign(response.url);
+    else if (mode == "Rank Top(ish) Movies") {
+        const autorankScore = window.parseInt(window.prompt('Select a score for autoranking score threshold'));
+        const listName = document.getElementById('userLists').value;
+        const response = await fetch('/rankListTopish', {
+            method: "POST",
+            redirect: 'follow',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ listName: listName, autorankThreshold: autorankScore }),
+        });
+        if (response.ok) {
+            if (response.redirected) {
+                window.location.assign(response.url);
+            }
         }
     }
-});
-
-
-document.getElementById('rankLeastButton').addEventListener('click', async () => {
-    const listName = document.getElementById('userLists').value;
-    const response = await fetch('/rankListLeast', {
-        method: "POST",
-        redirect: 'follow',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ listName: listName }),
-    });
-    if (response.ok) {
-        if (response.redirected) {
-            window.location.assign(response.url);
+    else if (mode == "Rank Least Ranked Movies") {
+        const listName = document.getElementById('userLists').value;
+        const response = await fetch('/rankListLeast', {
+            method: "POST",
+            redirect: 'follow',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ listName: listName }),
+        });
+        if (response.ok) {
+            if (response.redirected) {
+                window.location.assign(response.url);
+            }
         }
     }
-});
+    else if (mode == "Rename List") {
+        const listName = document.getElementById('userLists').value;
+        const newListName = window.prompt("Enter new list name");
+        if (newListName !== null && newListName.length > 0) {
+            const response = await fetch('/renameList', {
+                method: "POST",
+                redirect: 'follow',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ oldListName: listName, newListName: newListName }),
+            });
+            if (response.ok) {
+                const results = await response.json();
+                if (results['success']) {
+                    location.reload();
+                }
+                else {
+                    document.getElementById('confirmationMessage').innerHTML = results['msg'];
+                }
+            }
+        }
+    }
+    else if (mode == "Reset List") {
+        const listName = document.getElementById('userLists').value;
+        if (window.confirm("Reset " + listName)) {
+            await fetch('/resetList', {
+                method: "POST",
+                redirect: 'follow',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ listName: listName }),
+            });
+        }
+    }
+    else if (mode == "Delete List") {
+        const listName = document.getElementById('userLists').value;
+        if (window.confirm("Delete " + listName)) {
+            await fetch('/deleteList', {
+                method: "POST",
+                redirect: 'follow',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ listName: listName }),
+            });
+            location.reload();
+        }
+    }
+    else {
+        console.log("Something went wrong...")
+    }
+})
